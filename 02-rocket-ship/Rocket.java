da39931fa3979e93e04a1129c595c966f4d5fe05
DannyNgLiu <dannyn5@nycstudents.net>
2021-09-27 15:56:10 -0400

    added the code to print out the rocketship

---
+++ 02-rocket-ship/Rocket.java
@@ -0,0 +1,25 @@
+public class Rocket {
+  public static void main(String[] args) {
+//prints the rocketship with the five mthods below
+upsidedownv();
+box();
+text();
+box();
+upsidedownv();
+} //ends Rocket method
+  public static void upsidedownv() {
+    System.out.println( "   /\\ ");
+    System.out.println( "  /  \\ ");
+    System.out.println( " /    \\ ");
+  } //ends upsidedownv method
+  public static void box() {
+    System.out.println("+------+");
+    System.out.println("|      |");
+    System.out.println("|      |");
+    System.out.println("+------+");
+  } //ends box method
+  public static void text() {
+    System.out.println("|United|");
+    System.out.println("|States|");
+  } //ends text method
+} //ends main class
