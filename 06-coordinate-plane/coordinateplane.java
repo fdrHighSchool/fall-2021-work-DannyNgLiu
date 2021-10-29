7a333bc58ceb524a82f606eb219ac4ef4f3b5e28
DannyNgLiu <dannyn5@nycstudents.net>
2021-10-29 17:52:51 -0400

    finished coordinateplane for taxicabdistance and crowdistance

--- 06-coordinate-plane/coordinateplane.java
+++ 06-coordinate-plane/coordinateplane.java
@@ -10,16 +10,27 @@ public class coordinateplane {
   int x2 = xVal(point2);
   int y1 = yVal(point1);
   int y2 = yVal(point2);
-  System.out.println(x1);
+  double crowDis = crowDistance(x1, y1, x2, y2);
+  System.out.println(crowDis);
+  int taxiDis = taxicabDistance(x1, y1, x2, y2);
+  System.out.println(taxiDis);
 }
-    public static void xVal(String xPoint) {
+    public static int xVal(String xPoint) {
     int indexOfComma = xPoint.indexOf(",");
     int x = Integer.parseInt(xPoint.substring(1, indexOfComma));
     return x;
     }
-    public static void yVal(String yPoint) {
+    public static int yVal(String yPoint) {
     int indexOfComma = yPoint.indexOf(",");
     int y = Integer.parseInt(yPoint.substring(indexOfComma+2, yPoint.length()-1));
     return y;
+    }
+    public static double crowDistance(int x1, int y1, int x2, int y2) {
+      double crow = Math.sqrt(Math.pow((y2 - y1), 2) + Math.pow((x2 - x1), 2));
+      return crow;
+    }
+    public static int taxicabDistance(int x1, int y1, int x2, int y2) {
+      int taxicab = Math.abs(y2 - y1) + Math.abs(x2 - x1);
+      return taxicab;
     }
   }
