143ae7c8832440886057f51a7992365c482bbcc7
DannyNgLiu <dannyn5nycstudents.net>
2021-11-01 08:31:21 -0400

    fixed git pulling and pushing

--- 06-coordinate-plane/coordinateplane.java
+++ 06-coordinate-plane/coordinateplane.java
@@ -10,7 +10,10 @@ public class coordinateplane {
   int x2 = xVal(point2);
   int y1 = yVal(point1);
   int y2 = yVal(point2);
-
+  double crowDis = crowDistance(x1, y1, x2, y2);
+  System.out.println(crowDis);
+  int taxiDis = taxicabDistance(x1, y1, x2, y2);
+  System.out.println(taxiDis);
 }
     public static int xVal(String xPoint) {
     int indexOfComma = xPoint.indexOf(",");
@@ -22,10 +25,12 @@ public class coordinateplane {
     int y = Integer.parseInt(yPoint.substring(indexOfComma+2, yPoint.length()-1));
     return y;
     }
-    public static float crowDistance(int x1, int y1, int x2, int y2) {
-      float crowD = ((y2 - y1) + (x2 - x1));
+    public static double crowDistance(int x1, int y1, int x2, int y2) {
+      double crow = Math.sqrt(Math.pow((y2 - y1), 2) + Math.pow((x2 - x1), 2));
+      return crow;
     }
-    public static int taxicabDistance() {
-
+    public static int taxicabDistance(int x1, int y1, int x2, int y2) {
+      int taxicab = Math.abs(y2 - y1) + Math.abs(x2 - x1);
+      return taxicab;
     }
   }
