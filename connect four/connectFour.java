094a698d13aeb4bb1d2f34076f4987a4971885e7
DannyNgLiu <dannyn5@nycstudents.net>
2022-01-18 00:57:00 -0500

    need to let players alternate

--- connect four/connectFour.java
+++ connect four/connectFour.java
@@ -4,17 +4,20 @@ public class connectFour {
   public static void main(String[] args) {
     String[][] board = new String[6][7];
 
+    int turn = 1;
     //System.out.println(Arrays.deepToString(board));
     fillBoard(board);
     displayBoard(board);
-    // while(1 == 1) {
+    while(1 == 1) {
     Scanner s = new Scanner(System.in);
     System.out.println("Whcih column would you like to place your piece?");
     int userColumn = s.nextInt();
-
-   placePieces(userColumn, board);
+    System.out.println("turn is " + turn);
+   placePieces(userColumn, board, turn);
+       System.out.println("this is turn " + turn);
    displayBoard(board);
- // }
+       System.out.println("this is turn " + turn);
+    }
   } // end main method
 
   public static void fillBoard(String[][] board) {
@@ -34,24 +37,27 @@ public class connectFour {
     } // end outer for loop
   } // end displayBoard method
 
-  public static void placePieces(int userColumn, String[][] board) {
-    int turn = 1;
+  public static void placePieces(int userColumn, String[][] board, int turn) {
     int temp = 5;
+    System.out.println("this is turn " + turn);
     if(turn == 1) {
-      while(board[temp][userColumn] != "[ ]") {
+      while(board[temp][userColumn-1] != "[ ]") {
         temp--;
       }
-      board[temp][userColumn] = "[X]";
+      board[temp][userColumn-1] = "[X]";
       turn = 2;
+      System.out.println("turn equals " + turn);
       temp = 5;
+      return;
     }
     if(turn == 2) {
-      while(board[temp][userColumn] != "[ ]") {
+      while(board[temp][userColumn-1] != "[ ]") {
         temp--;
       }
       board[temp][userColumn-1] = "[O]";
       turn = 1;
       temp = 5;
+      return;
     }
   }
 } // end class
